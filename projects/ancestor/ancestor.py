00f12ade7b5f2eb3c9d20954264b3d9b4a3ab6bc
Ben <bigriffin94@outlook.com>
2020-06-10 13:49:53 -0700

    ancestor TRUTHFULLY passing OK now previous issue was my graph was flipped upside down

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -30,51 +30,69 @@ class Graph:
         """
         return self.vertices[vertex_id]
 
-    def earliest_ancestor(self, ancestors, starting_node):
-        """
-        USING A BREADTH FIRST SEARCH APPROACH
-        """
-        # we are using a BFS approach so we need to use a q
-        storage = Queue()
 
-        # we need to add our starting node to front of Q
-        # we do not make our added item a list bc we are returning a value of a node only
-        storage.enqueue([starting_node])
-        # print('fasfsadf', storage.size())
+def earliest_ancestor(ancestors, starting_node):
+    """
+    USING A BREADTH FIRST SEARCH APPROACH
+    """
+    # create our graph for every time we call this function on a family tree
+    graph = Graph()
 
-        # we need to create a set() visited var to track our path
-        visited = set()
+    # map our graph using the graph class by adding vertexes to the graph
+    for i in ancestors:
+        # print(i)
+        # plot first value of tuple in ancestors to the graph
+        graph.add_vertex(i[0])
+        # plot second value of tuple in ancestors to the graph
+        graph.add_vertex(i[1])
 
-        result = []
+        # graph.add_edge(x[1], x[0]) TODO + add vars == child/ parent for readability and comprehension
 
-        while storage.size() > 0:
-            # pop off the starting node of our Q to create a path
-            path = storage.dequeue()
-            # print('path after dequeue', path)
+    # map our edges to their respected nodes using the ancestors list of tuples
+    for x in ancestors:
+        graph.add_edge(x[1], x[0])
 
-            # >> grab the last vertex in the path bc that is the furthest point from beginning var
-            last_vert = path[-1]
-            # print('last_vert', last_vert)
+    # we are using a BFS approach so we need to use a q
+    storage = Queue()
 
-            # check if that last vertex is in our visited set()
-            if last_vert not in visited:
-                visited.add(last_vert)
+    # we need to add our starting node to front of Q
+    # we do not make our added item a list bc we are returning a value of a node only
+    storage.enqueue([starting_node])
+    # print('fasfsadf', storage.size())
 
-            # check if its the smallest value if there are two parents
-            # return the parent with smallest numeric id
-            for next_vert in self.get_neighbors(last_vert):
-                new_path = list(path)
-                new_path.append(next_vert)
-                storage.enqueue(new_path)
-                # print('New PAth', new_path)
-                result.append(new_path[-1])
+    # we need to create a set() visited var to track our path
+    visited = set()
 
-            if result == []:
-                return -1
+    result = []
 
-        # print('VISITED', visited)
-        # print('result', result)
-        return result[-1]
+    while storage.size() > 0:
+        # pop off the starting node of our Q to create a path
+        path = storage.dequeue()
+        # print('path after dequeue', path)
+
+        # >> grab the last vertex in the path bc that is the furthest point from beginning var
+        last_vert = path[-1]
+        # print('last_vert', last_vert)
+
+        # check if that last vertex is in our visited set()
+        if last_vert not in visited:
+            visited.add(last_vert)
+
+        # check if its the smallest value if there are two parents
+        # return the parent with smallest numeric id
+        for next_vert in graph.get_neighbors(last_vert):
+            new_path = list(path)
+            new_path.append(next_vert)
+            storage.enqueue(new_path)
+            # print('New PAth', new_path)
+            result.append(new_path[-1])
+
+        if result == []:
+            return -1
+
+    # print('VISITED', visited)
+    # print('result', result)
+    return result[-1]
 
 
 # GRAPH DESIGN
@@ -115,10 +133,11 @@ class Graph:
 #     graph.add_edge(8, 11)
 #     graph.add_edge(9, 8)
 
-# test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
-#                   (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+                  (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+print(earliest_ancestor(test_ancestors, 2))
 # print(graph.vertices)
-# print(graph.earliest_ancestor(test_ancestors, 1))
+# print(graph.earliest_ancestor(test_ancestors, 2))
 
 
 """
